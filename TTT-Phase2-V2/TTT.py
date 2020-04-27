898a498eb03b44195dd52e319e3ee48445d93abe
Kazi Jamal <jamalkazi02@gmail.com>
2020-04-27 14:56:23 -0400

    Update TTT-Phase2

--- TTT-Phase2-V2/TTT.py
+++ TTT-Phase2-V2/TTT.py
@@ -1,5 +1,14 @@
 # TicTacToe - perfect competitor code outline
 
+import sys
+
+Usage = '''Usage:
+python3 TTT-Random-NetLogo.py {result-filename} {board}
+'''
+
+Positions = ('Top-left','Top-center','Top-right','Middle-left','Middle-center',\
+             'Middle-right','Bottom-left','Bottom-center','Bottom-right')
+
 # Below, any  variable called "board" contains a board layout string of 9 chars or 'x', 'o' and '_'
 # AllBoards is a dictionary of all boards
 # key = board, value = the Tboard instance
@@ -83,14 +92,50 @@ def FindAllBoards(board_node):
             board_node.children.append(child_node)
             FindAllBoards(child_node)
     return
+
+def minimax(board_node, maximize, moves):
+    state = board_node.state
+    if state is not None:
+        if state == board_node.player:
+            return [1, moves, board_node]
+        elif state == board_node.opponent:
+            return [-1, moves, board_node]
+        elif state == 'd':
+            return [0, moves, board_node]
+    else:
+        if maximize:
+            best_score = -2
+            best_move = None
+            for child in board_node.children:
+                curr_score = minimax(child, False, moves + 1)
+                if curr_score[0] > best_score:
+                    best_score = curr_score[0]
+                    best_move = curr_score[2]
+            return [best_score, curr_score[1], best_move]
+        else:
+            worst_score = 2
+            worst_move = None
+            for child in board_node.children:
+                curr_score = minimax(child, True, moves + 1)
+                if curr_score[0] < worst_score:
+                    worst_score = curr_score[0]
+                    worst_move = curr_score[2]
+            return [worst_score, curr_score[1], worst_move]
+        
         
 def CalcBestMove(board_node):
     '''  updates this board_node with correct values for state, moves_to_state, and best_move
     (This is the engine.)'''
+    best_score = -2
+    best_move = None
+    for child in board_node.children:
+        curr_score = minimax(child, True, 0)
+        if curr_score[0] > best_score:
+            best_score, best_moves, best_move = curr_score
+    board_node.state = best_move.state
+    board_node.moves_to_state = best_moves
+    board_node.best_move = best_move.lastmove
 
-    ##############################################
-    #   Your excellent code here
-    ##############################################
         
 def WhoseMove(board):
     '''returns the player (either 'x' or 'o') and also opponent'''
@@ -117,7 +162,21 @@ def PrintBoardNode(node):
     for child_node in node.children:
         print('child',child_node.lastmove,child_node.board)
 
+def main():
+    if len(sys.argv) != 3:
+        print(Usage)
+        return
+    outfile = sys.argv[1]
+    board = sys.argv[2]
+    best_move, moves_to_state, state = FigureItOut(board)
+    print("best_move:", best_move)
+    print("moves_to_state:", moves_to_state)
+    print("state:", state)
+    s = '%d\n' % best_move
+    s += '%s\n' % Positions[best_move]
+    s += '%s in %d moves' % (state, moves_to_state)
+    f = open(outfile,'w')
+    f.write(s)
+    f.close()
 
-    
-    
-    
+main()
